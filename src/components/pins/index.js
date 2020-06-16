170941e32501970e89cc18fbe0830e94e59a1d3f
Gajendra Rajput <Gajendra.rajput@tcs.com>
2020-06-16 12:19:36 +0530

    refactor: Simplify the display pins logic

--- src/components/pins/index.js
+++ src/components/pins/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./index.css";
 
 const Pins = (props) => {
-  return (
-    <div className="Container">
-      <button id="pin0" onClick={() => props.pinsDown(0)}>
-        0
-      </button>
-      <button id="pin1" onClick={() => props.pinsDown(1)}>
-        1
-      </button>
-    </div>
-  );
+  const getPins = () => {
+    const pins = [];
+    for (let i = 0; i < 2; i++) {
+      pins.push(
+        <button id={"pin" + i} key={i} onClick={() => props.pinsDown(i)}>
+          {i}
+        </button>
+      );
+    }
+
+    return pins;
+  };
+  return <div className="Container">{getPins()}</div>;
 };
 
 export default Pins;
